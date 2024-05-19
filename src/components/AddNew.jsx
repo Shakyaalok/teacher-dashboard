a8eeba99f04918cb7fc44360663f7850490f0a3b
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-05-19 17:31:56 +0530

    add new reducer added

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useContext,useState } from 'react'
 import Modal from '../modals/Modals';
 import { Card ,Button,Form} from 'react-bootstrap'
-import './AddNew.css'
+import './AddNew.css';
+import studentContext from '../store/student-context'
+import axios from 'axios'
+
 
 const AddNew = (props) => {
+
+  const [name,setName] = useState('');
+  const [subject,setSubject] = useState('');
+  const [marks,setMarks] = useState('')
+
+  const studentCtx = useContext(studentContext);
+  console.log(studentCtx,'sfsf')
 
   //closing the add form
   const closeHandler = () =>{
       props.onCloseAdd(false);
+  }
+
+  const nameHandler = (e)=>{
+   setName(e.target.value)
+  }
+
+  const subjectHandler = (e) =>{
+   setSubject(e.target.value)
+  }
+
+  const marksHandler = (e) =>{
+     setMarks(e.target.value)
+  }
+
+  const addnewHandler = async(e)=>{
+    e.preventDefault();
+
+    const data = {
+      name:name,
+      subject:subject,
+      marks:marks
+    }
+  
+    const token = localStorage.getItem('token')
+    const res = await axios.post('http://localhost:8000/student/add',data,{headers:{Authorization:token}});
+    if(res.status===201){
+      console.log(res)
+      // const newStudent = res.data.data;
+      // if (!studentCtx.students.some(existingStudent => existingStudent.id !== newStudent.id)) {
+      //   studentCtx.addStudent(newStudent);
+      // }
+      studentCtx.addStudent(res.data.data)
+      closeHandler();
+    }
+
+    setName('');
+    setSubject('');
+    setMarks('');
+
+
+    console.log(data)
+
   }
 
   return (
@@ -18,22 +70,22 @@ const AddNew = (props) => {
         <Card.Title className='text-center add_heading'>Add New Records</Card.Title>
         <Button className='cross_button' onClick={closeHandler} >X</Button>
 
-        <Form>
+        <Form onSubmit={addnewHandler}>
         <Form.Group className="mb-3" >
           <Form.Label className='add'>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" className='add_input' />
+          <Form.Control type="text" placeholder="Enter Name" className='add_input' value={name} onChange={nameHandler} />
         </Form.Group> 
 
         <Form.Group className="mb-3" >
           <Form.Label className='add'>Subject</Form.Label>
-          <Form.Control type="text" placeholder="Enter Subject" className='add_input' />
+          <Form.Control type="text" placeholder="Enter Subject" className='add_input' value={subject} onChange={subjectHandler} />
         </Form.Group> 
 
         <Form.Group className="mb-3" >
           <Form.Label className='add'>Marks</Form.Label>
-          <Form.Control type="number" placeholder="Enter Marks" className='add_input'/>
+          <Form.Control type="number" placeholder="Enter Marks" className='add_input' value={marks} onChange={marksHandler}/>
         </Form.Group> 
-        <Button className='form_button add_button' style={{width:'100%'}}>Submit</Button>
+        <Button className='form_button add_button' style={{width:'100%'}} type='submit'>Submit</Button>
         </Form>
 
 
--- src/store/StudentProvider.jsx
+++ src/store/StudentProvider.jsx
@@ -1,61 +1,67 @@
-import React,{useEffect, useState} from 'react'
-import StudentContext from './student-context'
+import React, { useEffect, useState, useReducer } from "react";
+import StudentContext from "./student-context";
+
+const initalState = {
+  students: [],
+};
+
+const studentReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const existingStudentIndex = state.students.findIndex(
+      (student) => student.id === action.student.id
+    );
+
+    let updatedStudents;
+    if (existingStudentIndex !== -1) {
+      updatedStudents = state.students.map((student, index) =>
+        index === existingStudentIndex
+          ? { ...student, marks: action.student.marks }
+          : student
+      );
+    } else {
+      updatedStudents = [...state.students, action.student];
+    }
 
 
-// const initalState = {
-//   students:[]
-// }
+    return {
+      ...state,
+      students: updatedStudents,
+    };
+  }
 
-// const studentReducer = (state,action) =>{
-//  if(action.type==='ADD'){
-//   //  const students = [...state.students];
-//   //  return students;
-//   console.log('ADDs');
-//   return
-//   // return {
-//   //   ...state,
-//   //   students: [...state.students, action.student]
-//   // };
-//  }
-// }
+ 
 
 
+  return initalState;
+};
 
 const StudentProvider = (props) => {
-  const [students, setStudents] = useState([]);
+  // const [students, setStudents] = useState([]);/
 
+  const [studentState, dispatcher] = useReducer(studentReducer, initalState);
 
-  // const [studentState,dispatcher] = useReducer(studentReducer,initalState);
-
-  // const addStudentHandler = (student)=>{
-  //   console.log('students----------->',student)
-  //    dispatcher({type:'ADD',student:student})
-  // }
+  const addStudentHandler = (student) => {
+    console.log("students----------->", student);
+    dispatcher({ type: "ADD", student: student });
+  };
 
   // const removeStudentHandler = (id)=>{
 
   // }
 
-  const addStudentHandler = (student)=>{
-    setStudents((prevState)=>[...prevState,student]);
-  }
 
-  useEffect(()=>{
-    console.log("students",students)
-  },[students,addStudentHandler])
 
-    const context = {
-        students:students,
-        addStudent:addStudentHandler,
-        removeStudent:(id)=>{},
-        message:'this is working or not'
-
-    }
+  const context = {
+    students: studentState.students,
+    addStudent: addStudentHandler,
+    removeStudent: (id) => {},
+    message: "this is working or not",
+  };
   return (
     <StudentContext.Provider value={context}>
-         {props.children}
+      {props.children}
     </StudentContext.Provider>
-  )
-}
+  );
+};
 
-export default StudentProvider
+export default StudentProvider;
